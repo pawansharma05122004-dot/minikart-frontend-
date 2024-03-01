4861a872247505c3a89b67882fe01d48174e5e16
Ta-Run <tarnsharma11091999@gmail.com>
2024-03-02 01:53:48 +0530

    itengraete the buy now api

--- src/components/Main/BuyNow.js
+++ src/components/Main/BuyNow.js
@@ -1,25 +1,49 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
+
 function BuyNow() {
-    const {productId}= useParams()
-    console.log(productId)
-    useEffect(()=>{
+    const { productId } = useParams();
+    const [item, setItem] = useState(null);
+
+    useEffect(() => {
         const userObject = JSON.parse(localStorage.getItem('user'));
-        let UserID=userObject.user._id
+        const UserID = userObject.user._id;
 
-          const getBuyDetails=async()=>{
-           const result = await axios.post('http://localhost:8000/api/v1/minikart/purchase/getPurchaseItem',{
-              UserID,productId
-            })
-            console.log(result)
-          }
+        const getBuyDetails = async (body) => {
+            try {
+                const result = await axios.post('http://localhost:8000/api/v1/minikart/purchase/getPurchaseItem', body);
+                setItem(result.data.result);
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        };
 
-          getBuyDetails()
-    })
-  return (
-    <div>BuyNow</div>
-  )
+        getBuyDetails({
+            purchaseUser: UserID,
+            productDetails: productId
+        });
+    }, [productId]);
+
+    return (
+        <div>
+            <div className='bg-blue-400'>
+                <h1>Item Ordered Successfully</h1>
+                <div className='border bg-green-300 rounded-md'>
+                    {item ? (
+                        <>
+                            <ul>Email: {item.purchaseUser.email}</ul>
+                            <ul>Name: {item.purchaseUser.name}</ul>
+                            <ul>Product Name: {item.productDetails.product_name}</ul>
+                            <ul>Price: {item.productDetails.price}</ul>
+                        </>
+                    ) : (
+                        <p>Please wait...</p>
+                    )}
+                </div>
+            </div>
+        </div>
+    );
 }
 
-export default BuyNow
+export default BuyNow;
