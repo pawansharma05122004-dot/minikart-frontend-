f131ce981a86bbbfd5f1d9a5ee8b13e8ac263db3
unknown <tarunsharma11091999@gmail.com>
2024-03-23 12:21:29 -0700

    impliment the header and footer

--- src/components/Main/Header.jsx
+++ src/components/Main/Header.jsx
@@ -10,10 +10,10 @@ function Header() {
     navigate('/')
   }
   return (
-    <header className='shadow sticky z-50 top-0'>
-      <nav className='bg-white border-gray-200 px-4 lg:px-6 py-2.5'>
+    <header className='shadow sticky z-50 top-0 '>
+      <nav className='bg-blue-700 border-gray-200 px-4 lg:px-6 py-2.5'>
         <div className='flex flex-wrap justify-between items-center mx-auto'>
-          <button onClick={handleNavigate} className='font-serif font-bold'>Minikart</button>
+          <button onClick={handleNavigate} className='font-serif font-bold text-white'>Minikart</button>
           <Searchbar/>
           <Navbar/>
         </div>
--- src/components/Main/Navbar.jsx
+++ src/components/Main/Navbar.jsx
@@ -16,26 +16,26 @@ function Navbar() {
   return (
     <header >
 
-      <div className='flex  justify-between items-center mx-auto space-x-4'>
+      <div className='flex  justify-between items-center mx-auto space-x-4 '>
         <li className='flex  justify-between items-center mx-auto space-x-4'>
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
             <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
           </svg>
           <NavLink to='/addToCart' className={({ isActive }) =>
-            `${isActive ? "text-orange-600" : "hover:text-green-600 "} font-bold`
+            `${isActive ? "text-orange-600" : "hover:text-green-600 "} font-bold text-white`
           }>
             Check Cart</NavLink>
         </li>
 
         <li className='flex  justify-between items-center mx-auto'>
           <NavLink to='/postProduct' className={({ isActive }) =>
-            `${isActive ? "text-orange-600" : "hover:text-green-600 "} font-bold`
+            `${isActive ? "text-orange-600" : "hover:text-green-600 "} font-bold text-white`
           }>Become a Seller</NavLink>
         </li>
 
         <li className='flex  justify-between items-center mx-auto'>
           <NavLink to='/signup' className={({ isActive }) =>
-            `${isActive ? "text-orange-600" : "hover:text-green-600 "} font-bold`
+            `${isActive ? "text-orange-600" : "hover:text-green-600 "} font-bold text-white`
           }>SignUp</NavLink>
         </li>
 
@@ -45,7 +45,7 @@ function Navbar() {
           </svg>
 
           {userObject.user ? (
-            <span onClick={handleLogout} className='cursor-pointer font-bold'>
+            <span onClick={handleLogout} className='cursor-pointer font-bold text-white'>
               Log Out
             </span>
           ) : (
