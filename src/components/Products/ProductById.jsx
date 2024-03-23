1743223eb5f75583030dd8d536a8887c53244607
unknown <tarunsharma11091999@gmail.com>
2024-03-23 13:24:52 -0700

    imporve ui of view items

--- src/components/Products/ProductById.jsx
+++ src/components/Products/ProductById.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useParams, Link } from 'react-router-dom';
+import { Spinner } from "@material-tailwind/react";
 
 function ProductById() {
     const { productId } = useParams();
@@ -13,12 +14,6 @@ function ProductById() {
                 // Fetch the product details by ID
                 const productResponse = await axios.post(`${process.env.REACT_APP_API_URL}/products/getProductById`, { productById: productId });
                 setProduct(productResponse.data.result);
-
-                // Fetch similar products based on product type
-                if (productResponse.data.result.product_type) {
-                    const similarResponse = await axios.post(`${process.env.REACT_APP_API_URL}/products/getSimilarProducts`, { productType: productResponse.data.result.product_type });
-                    setSimilarProducts(similarResponse.data.result);
-                }
             } catch (error) {
                 console.error('Error fetching data:', error);
             }
@@ -28,51 +23,48 @@ function ProductById() {
     }, [productId]);
 
     return (
-        <div className="flex flex-col items-center py-8">
-            {/* Render product details */}
+        <div className="container mx-auto mt-8 px-4">
             {product ? (
-                <>
-                    <div className="max-w-lg p-6 bg-white shadow-md rounded-md mb-8">
-                        <img src={product.product_img} alt='product' className='h-72 w-72 mx-auto mb-4'/>
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+                    <div className="bg-white shadow-md rounded-md">
+                        <img src={product.product_img} alt='product' className='w-full h-auto object-cover rounded-t-md' />
+
+                    </div>
+                    <div className="p-6 flex flex-col justify-center  bg-white shadow-md rounded-md">
                         <h2 className="text-2xl font-semibold mb-2">{product.product_name}</h2>
-                        <p className="text-lg text-gray-600 mb-4">Price: ${product.price}</p>
-                        <Link to={`/orderAddress/${product._id}`}>
-                            <button className='block w-full py-2 px-4 bg-green-600 text-white rounded-md hover:bg-green-700 focus:outline-none focus:bg-green-700'>
-                                Buy Now
-                            </button>
-                        </Link>
+                        <p className="text-lg text-gray-600  font-semibold mb-4">Price: ${product.price}</p>
+                        <p className="text-lg text-gray-600 font-semibold mb-4">Discount: {product.discount}</p>
+                        <p className="text-lg text-gray-600 font-semibold mb-4">Brand: {product.brand}</p>
+                        <p className="text-lg text-gray-600 font-semibold font-semibold mb-4">Warranty: 2 years</p>
+                        <p className="text-lg text-gray-600 font-semibold mb-4">Description: {product.product_description}</p>
+                        <div className='flex flex-row justify-items-center items-center'>
+                            <Link to={`/orderAddress/${product._id}`}>
+                                <button className='block w-full py-2 px-4 bg-green-600 text-white rounded-md hover:bg-green-700 focus:outline-none focus:bg-green-700'>
+                                    Buy Now
+                                </button>
+                            </Link>
+                            <Link to={`/orderAddress/${product._id}`}>
+                                <button className='block w-full py-2 px-4 bg-blue-700 text-white rounded-md hover:bg-green-700 focus:outline-none focus:bg-green-700'>
+                                    Add Cart
+                                </button>
+                            </Link>
+                        </div>
                     </div>
 
-                    {/* Render customer reviews */}
-                    <div className="max-w-lg p-6 bg-white shadow-md rounded-md mb-8">
-                        <h2 className="text-2xl font-semibold mb-4">Customer Reviews</h2>
-                        {/* Add your review section here */}
+                    <div className="bg-white shadow-md rounded-md p-6">
+                        <h2 className="text-2xl font-semibold mb-4">Thumbmail</h2>
                         <p className="text-lg text-gray-600">No reviews yet</p>
                     </div>
+                    <div className="bg-white shadow-md rounded-md p-6">
+                        <h2 className="text-2xl font-semibold mb-4">Customer Reviews</h2>
+                        <p className="text-lg text-gray-600">No reviews yet</p>
+                    </div>
+                </div>
 
-                    {/* Render similar products */}
-                    {/* {similarProducts.length > 0 && (
-                        <div className="max-w-lg p-6 bg-white shadow-md rounded-md">
-                            <h2 className="text-2xl font-semibold mb-4">Similar Products</h2>
-                            <div className="grid grid-cols-2 gap-4">
-                                {similarProducts.map(similarProduct => (
-                                    <div key={similarProduct._id} className="flex flex-col bg-gray-100 p-4 rounded-md shadow-md">
-                                        <img src={similarProduct.product_img} alt={similarProduct.product_name} className="h-40 w-full object-contain mb-2" />
-                                        <p className="text-lg font-semibold">{similarProduct.product_name}</p>
-                                        <p className="text-lg text-gray-600">Price: ${similarProduct.price}</p>
-                                        <Link to={`/productbyid/${similarProduct._id}`} className="mt-auto">
-                                            <button className='block w-full py-2 px-4 bg-blue-600 text-white rounded-md hover:bg-blue-700 focus:outline-none focus:bg-blue-700'>
-                                                View Details
-                                            </button>
-                                        // </Link>
-                                    </div>
-                                ))}
-                            </div>
-                        </div>
-                    )} */}
-                </>
             ) : (
-                <p className="text-center">Loading...</p>
+                <div className="flex justify-center items-center h-screen">
+              <Spinner className="h-16 w-16 text-gray-900/50" />
+            </div>
             )}
         </div>
     );
