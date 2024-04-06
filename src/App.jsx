ce33f70915ff52655b45a2593aba1979c08c11b4
unknown <tarunsharma11091999@gmail.com>
2024-04-07 00:26:58 +0530

    add lazy laoding

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,43 @@
-import React from "react";
-import Footer from "./components/Main/Footer";
+import React, { Suspense, lazy } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./components/Main/Home";
 import Header from "./components/Main/Header";
-import SignUp from "./components/Authontication/SignUp";
-import Login from "./components/Authontication/LogIn";
-import OrderProduct from "./components/Products/OrderProduct";
-import ProductById from "./components/Products/ProductById";
-import PostProduct from "./components/Seller/PostProduct";
-import AddToCart from "./components/Cart/AddToCart";
-import { PaymentSuccess } from "./components/Payment/PaymentSuccess";
-import LoginUser from "./components/Helper/LoginUser";
-// import { ContextState } from "./components/Context/ContextState";
+import Footer from "./components/Main/Footer";
 import ContextState from "./components/Context/ContextState";
+
+const Home = lazy(() => import("./components/Main/Home"));
+const ProductById = lazy(() => import("./components/Products/ProductById"));
+const OrderProduct = lazy(() => import("./components/Products/OrderProduct"));
+const PostProduct = lazy(() => import("./components/Seller/PostProduct"));
+const AddToCart = lazy(() => import("./components/Cart/AddToCart"));
+const PaymentSuccess = lazy(() => import("./components/Payment/PaymentSuccess"));
+const SignUp = lazy(() => import("./components/Authontication/SignUp"));
+const Login = lazy(() => import("./components/Authontication/LogIn"));
+const LoginUser = lazy(() => import("./components/Helper/LoginUser"));
+
 function App() {
-    return (
+  return (
     <div className="flex flex-col min-h-screen">
       <ContextState>
-        <BrowserRouter >
+        <BrowserRouter>
           <Header />
           <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/productbyid/:productId' element={<ProductById />} />
-            <Route path='/OrderProduct' element={<OrderProduct />} />
-            <Route path='/postProduct' element={<PostProduct />} />
-            <Route path='/addToCart' element={<AddToCart />} />
-            <Route path='/paymentSuccess' element={<PaymentSuccess />} />
-            <Route path='/signup' element={<SignUp />} />
-            <Route path='/login' element={<Login />} />
-            <Route path='/loginUser' element={<LoginUser />} />
-            <Route path='*' element={<img src={'https://blog.fluidui.com/assets/images/posts/get-notes.png'} alt='notvalid' />} />
+            <Route
+              path="/"
+              element={
+                <Suspense fallback={<div>Loading...</div>}>
+                  <Home />
+                </Suspense>
+              }
+            />
+            <Route path="/productbyid/:productId" element={<ProductById />} />
+            <Route path="/OrderProduct" element={<OrderProduct />} />
+            <Route path="/postProduct" element={<PostProduct />} />
+            <Route path="/addToCart" element={<AddToCart />} />
+            <Route path="/paymentSuccess" element={<PaymentSuccess />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/loginUser" element={<LoginUser />} />
+            <Route path="*" element={<img src={'https://blog.fluidui.com/assets/images/posts/get-notes.png'} alt='notvalid' />} />
           </Routes>
           <Footer />
         </BrowserRouter>
